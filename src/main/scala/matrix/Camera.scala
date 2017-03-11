a47cadd27d4d0b01ec69e2f7d77415e527aa1370
mrthomaswagner@gmail.com <mrthomaswagner@gmail.com>
2017-03-11 18:56:34 -0500

    cleanup

--- src/main/scala/matrix/Camera.scala
+++ src/main/scala/matrix/Camera.scala
@@ -6,37 +6,39 @@ import javax.imageio.ImageIO
 
 import scala.collection.mutable.ArrayBuffer
 
-case class Pixel(r: Int, g: Int, b: Int)
+case class Pixel(r: Int, g: Int, b: Int) {
+  def color: Int = {
+    r * 65536 + g * 256 + b
+  }
+}
 
 class Camera(aspectRatio: Tuple2[Int, Int], depth: Int) {
   val origin: Point = Point(0, 0, 0)
   private val (width, height) = aspectRatio
 
-
-  val rows: ArrayBuffer[Pixel] = new ArrayBuffer[Pixel]()
-  (0 to height).foreach { i => rows += Pixel(i* (255/height), 0, 0) }
-  val film: ArrayBuffer[ArrayBuffer[Pixel]] = ArrayBuffer.fill(width, height) {
-   Pixel(0, 0, 0)
+  val film = ArrayBuffer.fill(width, height)(Pixel(0, 0, 0))
+  for (
+    x <- 0 until width;
+    y <- 0 until height
+  ) {
+    val green = x*(255.toDouble/width)
+    film(x)(y) = Pixel(0, green.toInt, 0)
   }
+
 
   def coordsFor(x: Int, y: Int): Point = {
     Point(x - width / 2, y - height / 2, -depth)
   }
 
-
   def write() = {
     val out = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-    (0 until width).foreach { x =>
-      (0 until height).foreach { y =>
-        val pixel = film(x)(y)
-
-        val red: Double = x*255.toDouble/width
-        val color:Int = red.toInt* 65536 + (pixel.g * 256) + pixel.b
-        out.setRGB(x, y, color)
-      }
+    for (
+      x <- 0 until width;
+      y <- 0 until height
+    ) yield {
+      out.setRGB(x, y, film(x)(y).color)
     }
-    val f = new File("/Users/tommy/Desktop/yay.jpeg")
-    ImageIO.write(out, "jpg", f)
+    ImageIO.write(out, "jpg", new File("/Users/tommy/Desktop/yay.jpeg"))
   }
 
 }
